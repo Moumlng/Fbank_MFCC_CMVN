f9e44f8eefbc771b7a7ada40de4d3909968cc87f
Moumlng <moumlng8062@gmail.com>
2023-10-02 22:35:53 +0800

    update extra 2&3

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import get_filter_bank
+from utils import get_filter_bank, MFCC_from_Fbank, diff_3
 
-fs, wav = scipy.io.wavfile.read('PHONE_001.wav')
+wave_name = 'PHONE_001.wav'
+fs, wav = scipy.io.wavfile.read(wave_name)
 print(f'wave length = {len(wav) / fs}s.')
 filter_id, t, Fbank = get_filter_bank(wav, fs, nperseg = 512)
-np.save('Fbank', Fbank)
+np.save(f'{wave_name}_Fbank', Fbank)
 
-#plt.show() #show mel bank filters and filter_bank
+c_id, MFCC = MFCC_from_Fbank(Fbank)
+np.save(f'{wave_name}_MFCC', MFCC)
+
+MFCC_diff_3 = diff_3(MFCC)
+mean = np.mean(MFCC_diff_3)
+std = np.std(MFCC_diff_3)
+MFCC_diff_3 = (MFCC_diff_3 - mean) / std
+np.save(f'{wave_name}_MFCC_diff_3', MFCC_diff_3)
+#plt.show()
--- utils.py
+++ utils.py
@@ -2,12 +2,23 @@ import scipy.signal as signal
 import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 def mel(freq):
     return 2595 * np.log10(1 + freq / 700)
 
 def imel(melfreq):
     return (10**(melfreq / 2595) - 1) * 700
+
+def diff_3(data):
+    data = np.array(data)
+    def diff_1(data):
+        column1 = data[:,0]
+        column1 = np.expand_dims(column1, axis=1)
+        data_extended = np.concatenate((column1, data), axis = 1)
+        data_diff = data_extended[:,1:]-data_extended[:,:-1]
+        return data_diff
+    return(diff_1(diff_1(diff_1(data))))
 
 def get_filter_bank(wav, fs, nperseg = 512, freq_range = [60, 3400], n_banks = 15, window = 'hann', miu = 0.95):
     
@@ -28,20 +39,61 @@ def get_filter_bank(wav, fs, nperseg = 512, freq_range = [60, 3400], n_banks = 1
     plt.legend([f'filter {i}' for i in range(n_banks)])
     zxx = (abs(zxx)) ** 2
     filter_bank = np.matmul(bank_filters, zxx)
+    filter_bank = np.log(filter_bank)
     filter_id = [i for i in range(n_banks)]
-    ff,tt = np.meshgrid(filter_id, t)
-    #print(bank_filter.shape, zxx.shape, filter_bank.shape)
+    
+    return filter_id, t, filter_bank
+
+def MFCC_from_Fbank(Fbank, nDCT=None):
+    
+    if nDCT == None:
+        nDCT = Fbank.shape[0]
+    else:
+        assert type(nDCT) == int, f'The nDCT shoule be int type, not {type(nDCT)} type.'
+        
+    DCT_matrix = [[np.cos(np.pi*(i + 1)/Fbank.shape[0]*(j + 0.5)) for j in range(Fbank.shape[0])]for i in range(nDCT)]
+    DCT_matrix = np.sqrt(2/Fbank.shape[0]) * np.array(DCT_matrix)
+    
+    MFCC = np.matmul(DCT_matrix, Fbank)
+    c_id = np.array([i+1 for i in range(nDCT)])
+    
+    return c_id, MFCC
+
+
+if __name__ == '__main__':
+    fs, wav = scipy.io.wavfile.read('PHONE_001.wav')
+    
+    fid, t, Fbank = get_filter_bank(wav, fs, nperseg = 512, n_banks=15)
+    
+    ff,tt = np.meshgrid(fid, t)
     plt.figure(figsize=[10,5])
-    plt.pcolor(tt.T[:,:100], ff.T[:,:100], np.log(filter_bank[:,:100]), cmap='Reds', shading='auto')
+    plt.pcolor(tt.T[:,500:600], ff.T[:,500:600], Fbank[:,500:600], cmap='Reds', shading='auto')
     plt.xlabel('time/s')
     plt.ylabel('filter number')
     plt.title('Filter bank Log Amptitude in the first few seconds')
     plt.colorbar()
     
-    return filter_id, t, filter_bank
-
-
-if __name__ == '__main__':
-    fs, wav = scipy.io.wavfile.read('PHONE_001.wav')
-    get_filter_bank(wav, fs, nperseg = 512, n_banks=15)
+    c_id, MFCC = MFCC_from_Fbank(Fbank)
+    cc,tt = np.meshgrid(c_id, t)
+    plt.figure(figsize=[10,5])
+    norm = colors.TwoSlopeNorm(vmin=MFCC.min(), vmax=MFCC.max(), vcenter=0)
+    plt.pcolor(tt.T[:,500:600], cc.T[:,500:600], MFCC[:,500:600], cmap='bwr', shading='auto', norm=norm)
+    plt.xlabel('time/s')
+    plt.ylabel('MFCC_id')
+    plt.title('MFCC')
+    plt.colorbar()
+    
+    MFCC_diff_3 = diff_3(MFCC)
+    mean = np.mean(MFCC_diff_3)
+    std = np.std(MFCC_diff_3)
+    MFCC_diff_3 = (MFCC_diff_3 - mean) / std
+    cc,tt = np.meshgrid(c_id, t)
+    plt.figure(figsize=[10,5])
+    norm = colors.TwoSlopeNorm(vmin=MFCC_diff_3.min(), vmax=MFCC_diff_3.max(), vcenter=0)
+    plt.pcolor(tt.T[:,:100], cc.T[:,:100], MFCC_diff_3[:,:100], cmap='bwr', shading='auto', norm=norm)
+    plt.xlabel('time/s')
+    plt.ylabel('MFCC_id')
+    plt.title('MFCC_diff3')
+    plt.colorbar()
+    
     plt.show()
